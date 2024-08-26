3ba89ecc87a3eeecb342ce9675decb4907da622d
acidtib <arubinofaux@gmail.com>
2024-08-25 19:37:47 -0600

    fix bug preventing init from generating config.yml

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,14 +1,10 @@
 import {
   parseYaml,
   resolve,
-  logger
 } from "../deps.ts";
 
 class Config {
-  private data: Record<string, any>;
-
   constructor() {
-    this.data = this.loadSync();
   }
 
   private loadSync(): Record<string, any> {
@@ -17,16 +13,16 @@ class Config {
       const configContent = Deno.readTextFileSync(configPath);
       return parseYaml(configContent) as Record<string, any>;
     } catch (error) {
-      logger.error(`Failed to load configuration from ${configPath}:`, error);
-      throw new Error("Failed to load configuration");
+      throw new Error("Failed to load configuration from ${configPath}", error);
     }
   }
 
   // const useTransition = config.get<boolean>("use_transition"); // true or false
   // const introPath = config.get<string>("intro_path"); // "./assets/media/intro.mp4"
   get<T = any>(key: string): T | undefined {
+    const data = this.loadSync();
     const keys = key.split(".");
-    let result: any = this.data;
+    let result: any = data;
 
     for (const k of keys) {
       if (result && typeof result === "object" && k in result) {
