726bb2bae5ddccc73f02d2af6f9b81f6b5678620
acidtib <arubinofaux@gmail.com>
2025-06-06 00:10:09 -0600

    chore: update dependencies and imports in configuration files
    - Added new imports for @cliffy/ansi, @cliffy/command, and various std modules in deno.json and src/deps.ts.
    - Updated deno.lock to reflect new versions of dependencies, including zod and std modules.
    - Removed old import paths and replaced them with updated module references.

--- src/deps.ts
+++ src/deps.ts
@@ -1,15 +1,13 @@
-export { Command, HelpCommand } from "https://deno.land/x/cliffy@v1.0.0-rc.4/command/mod.ts";
+export { Command } from "@cliffy/command";
+export { HelpCommand } from "@cliffy/command/help";
+export { colors } from "@cliffy/ansi/colors";
 
-export { colors } from "https://deno.land/x/cliffy@v1.0.0-rc.4/ansi/colors.ts";
+export { resolve } from "@std/path";
+export { ensureDir, exists } from "@std/fs";
+export { ulid } from "@std/ulid"
+export { parse as parseYaml, stringify as stringifyYaml } from "@std/yaml";
 
-export { resolve } from "https://deno.land/std@0.224.0/path/mod.ts";
-export { ensureDir, exists } from "https://deno.land/std@0.224.0/fs/mod.ts";
-
-export { z } from "https://deno.land/x/zod@v3.21.4/mod.ts";
-export { ulid } from "https://deno.land/std@0.208.0/ulid/mod.ts"
-
-export { parse as parseYaml, stringify as stringifyYaml } from "jsr:@std/yaml";
-
+export { z } from "zod";
 
 // libs
 export { config } from "./lib/config.ts";
